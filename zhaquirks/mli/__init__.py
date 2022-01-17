b89fc607ae516587540d4337b06f5b029ef428c4
SimpleUserHA <76794676+SimpleUserHA@users.noreply.github.com>
2022-01-17 23:27:54 +0100

    added configuration for 2AA batteries

--- zhaquirks/mli/__init__.py
+++ zhaquirks/mli/__init__.py
@@ -1,1 +1,16 @@
 """Mueller Licht International."""
+from zhaquirks import PowerConfigurationCluster
+
+
+class PowerConfiguration2AAACluster(PowerConfigurationCluster):
+    """Updating Power attributes 2 AAA."""
+
+    BATTERY_SIZES = 0x0031
+    BATTERY_QUANTITY = 0x0033
+    BATTERY_RATED_VOLTAGE = 0x0034
+
+    _CONSTANT_ATTRIBUTES = {
+        BATTERY_SIZES: 4,
+        BATTERY_QUANTITY: 2,
+        BATTERY_RATED_VOLTAGE: 15,
+    }
